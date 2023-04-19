828f3bd1f0e3b60ad7addb8015598ba7fff63bb6
Ishwar749 <19itish083@ldce.ac.in>
2023-04-19 13:08:29 +0530

    Fixes with focus manager

--- app/src/main/java/com/example/diaryapp/presentation/screens/write/WriteContent.kt
+++ app/src/main/java/com/example/diaryapp/presentation/screens/write/WriteContent.kt
@@ -11,15 +11,20 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.diaryapp.model.Diary
 import com.example.diaryapp.model.Mood
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -35,6 +40,12 @@ fun WriteContent(
 ) {
     val scrollState = rememberScrollState()
     val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val focusManager = LocalFocusManager.current
+
+    LaunchedEffect(key1 = scrollState.maxValue) {
+        scrollState.scrollTo(scrollState.maxValue)
+    }
 
     Column(
         modifier = Modifier
@@ -82,7 +93,12 @@ fun WriteContent(
                     imeAction = ImeAction.Next
                 ),
                 keyboardActions = KeyboardActions(
-                    onNext = {}
+                    onNext = {
+                        scope.launch {
+                            scrollState.animateScrollTo(Int.MAX_VALUE)
+                        }
+                        focusManager.moveFocus(FocusDirection.Down)
+                    }
                 ),
                 maxLines = 1,
                 singleLine = true
@@ -104,7 +120,7 @@ fun WriteContent(
                     imeAction = ImeAction.Next
                 ),
                 keyboardActions = KeyboardActions(
-                    onNext = {}
+                    onNext = { focusManager.clearFocus() }
                 )
             )
         }
@@ -116,20 +132,20 @@ fun WriteContent(
                     .fillMaxWidth()
                     .height(54.dp),
                 onClick = {
-                          if(uiState.title.isNotEmpty() && uiState.description.isNotEmpty()){
-                              onSaveClicked(
-                                  Diary().apply {
-                                      this.title = uiState.title
-                                      this.description = uiState.description
-                                  }
-                              )
-                          } else {
-                              Toast.makeText(
-                                  context,
-                                  "Fields cannot be empty.",
-                                  Toast.LENGTH_SHORT
-                              ).show()
-                          }
+                    if (uiState.title.isNotEmpty() && uiState.description.isNotEmpty()) {
+                        onSaveClicked(
+                            Diary().apply {
+                                this.title = uiState.title
+                                this.description = uiState.description
+                            }
+                        )
+                    } else {
+                        Toast.makeText(
+                            context,
+                            "Fields cannot be empty.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 },
                 shape = Shapes().small
             ) {
